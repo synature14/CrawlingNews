4022c3eca03c8d602064b87a3bc24bcfc649ffd1
suyeon <synature14@gmail.com>
2018-10-23 13:30:26 +0900

    시드니의 부동산 홈페이지 파싱, 이제 가격/날짜/건물유형/공인중개사 등 세부 정보 파싱해야함

--- CrawlingWeather/CrawlingFunction/CrawlingViewController.swift
+++ CrawlingWeather/CrawlingFunction/CrawlingViewController.swift
@@ -8,27 +8,55 @@
 
 import UIKit
 import SwiftSoup
+import Alamofire
 
 class CrawlingViewController: UIViewController {
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        scrapHTML()
     }
     
+    func parseHTML(html: String) {
+        
+        do {
+            let doc: Document = try SwiftSoup.parse(html)
+
+            let link = try doc.getElementsByTag("div").filter {
+                try $0.className() == "tiered-results tiered-results--exact" }.first!
+            let presentationLink = try link.getElementsByTag("div").map {
+                try $0.attr("role", "presentation") }.first!
+            let articleLink: Element = try link.getElementsByTag("article").filter {
+                try $0.className() == "results-card residential-card"
+            }.first!
+            
+            
+//            let linkSpan: String = try link.attr("span")
+            let linkText: String = try articleLink.text() // "example""
+
+            print("articleLink : \(articleLink)")
+            print("linkText = \(linkText)")
+            
+        } catch Exception.Error(let type, let message) {
+            print("[ERROR] type : \(type), \(message)")
+        } catch {
+            print("error")
+        }
+    }
+    
+    func scrapHTML() {
+        let html = "https://www.realestate.com.au/buy/in-sydney,+nsw+2000%3b/list-1"
+        Alamofire.request(html).responseString(completionHandler: { response in
+            print("isSuccess : \(response.result.isSuccess)")
+            if let html = response.result.value {
+                self.parseHTML(html: html)
+            }
+        })
+    }
 
     static func create() -> CrawlingViewController {
         let sb = UIStoryboard(name: "Crawling", bundle: nil)
         return sb.instantiateViewController(withIdentifier: "CrawlingViewController") as! CrawlingViewController
-    }
-
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
 
 }
